73895959f79ca19bed8a126d2801898d1175be86
afquidangen <alyssafayequidangen@gmail.com>
2024-10-08 20:50:54 +0800

    If Statement Activity 1

--- ageCheck.js
+++ ageCheck.js
@@ -1,12 +1,12 @@
 function askAge () {
     let age = prompt("Enter your age: ");
     if (age < 13 && age > 0) {
-        alert('You are a child.');
+        alert("You are a child.");
     } else if (age >= 13 && age <= 19) {
-        alert("You are a teenager. ");
+        alert("You are a teenager.");
     } else if (age > 19) {
-        alert("You are an adult. ");
+        alert("You are an adult.");
     } else {
-        alert("Invalid. ");
+        alert("Invalid.");
     }
 }
